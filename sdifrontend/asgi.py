13d29182dcfd7dd14f1cef3b7636e120dea1d09a
Hyogi Sim <simh@ornl.gov>
2020-03-19 13:06:15 -0400

    openshift integration

--- sdifrontend/asgi.py
+++ sdifrontend/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hsdcsite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sdifrontend.settings')
 
 application = get_asgi_application()
