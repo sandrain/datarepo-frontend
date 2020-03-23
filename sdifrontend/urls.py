3f7e25a1b86dcebbe7ca1576205f352d7b637ff9
Lee, Sangkeun (Matt) <lees4@ornl.gov>
2020-03-23 16:01:18 -0400

    latest

--- sdifrontend/urls.py
+++ sdifrontend/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^mainpage/', include('mainpage.urls')),
+    url(r'', include('mainpage.urls')),
     url(r'^dbtest/', include('dbtest.urls')),
 ]
