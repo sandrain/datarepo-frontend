95470dd2660ade17082742129197501534aedda7
Lee, Sangkeun (Matt) <lees4@ornl.gov>
2020-04-08 16:47:17 -0400

    now can create a dataset from webpage, quicklook shows contents

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     #url(r'^$', views.index, name='index'),
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^(?P<dataset_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^update/', views.update, name='update'),
 
     ]
--- mainpage/views.py
+++ mainpage/views.py
@@ -5,14 +5,16 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 import json
+import uuid
 
 from .models import SysDataset
 from .models import SysUser
-
+import random
 
 # Create your views here.
 
 def unpack_dataset_json(dataset):
+    dataset.id = dataset.id
     dataset.attributes = json.loads(dataset.properties)
     dataset.sizemb = int(dataset.size / (2**20))
     dataset.filecount = len((json.loads(dataset.structure))['data'])
@@ -37,6 +39,23 @@ def detail(request, dataset_id):
     dataset = unpack_dataset_json(get_object_or_404(SysDataset.objects.select_related(), id=dataset_id))
     
     return render(request, 'mainpage/detail.html', {'dataset': dataset})
+
+def update(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+
+        post_data = request.POST
+        sysdataset_title = post_data['sysdataset_title']
+        uuid_str = str(uuid.uuid4())
+
+        d = SysDataset(properties='{"title":"'+sysdataset_title+'","keywords":["a","b","c"]}', uuid = uuid_str, owner = SysUser.objects.all()[random.randint(0,10)], size = random.randint(100000,20000000), structure='{"data":[{"name":"1.csv","size": 1024}]}')
+        d.save()
+
+        dataset = unpack_dataset_json(get_object_or_404(SysDataset.objects.select_related(), id=d.id))
+        return render(request, 'mainpage/update.html', {'dataset': dataset})
+
+    dataset = unpack_dataset_json(get_object_or_404(SysDataset.objects.select_related(), id=690))
+    return render(request, 'mainpage/update.html', {'dataset': dataset})
 
 #class DetailView(generic.DetailView):
 #    model = DataSet
