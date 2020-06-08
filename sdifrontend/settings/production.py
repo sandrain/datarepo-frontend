60ffcf2df1826d2a33ecc87e9752f192d3535ffa
Swen Boehm <boehms@users.noreply.github.com>
2020-06-08 15:18:06 -0400

    Fix some config for deployment

--- sdifrontend/settings/production.py
+++ sdifrontend/settings/production.py
@@ -27,9 +27,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'io4o1_(a2m_9%oe8hzmo_$y(h#+*^@j1x+lplp@zq20u75@=n_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-#COMPRESS_ENABLED = True
-#COMPRESS_OFFLINE = True
+COMPRESS_ENABLED = True
+COMPRESS_OFFLINE = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -210,6 +209,7 @@ STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesSto
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
     'sass_processor.finders.CssFinder'
 ]
 
