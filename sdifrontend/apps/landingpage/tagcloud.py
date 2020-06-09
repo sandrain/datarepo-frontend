d4299dec3cadeaff546074c445f5db78116fd3e8
Lee, Sangkeun (Matt) <lees4@ornl.gov>
2020-06-09 15:50:51 -0400

    added dynamic tag using search index

--- sdifrontend/apps/landingpage/tagcloud.py
+++ sdifrontend/apps/landingpage/tagcloud.py
@@ -1,13 +1,37 @@
+import json
 from django import template
-
+from sdifrontend.apps.mainpage.models import SearchIndex
+from sdifrontend.apps.mainpage.models import SysDataset
 register = template.Library()
 
-class TagCloud:
-    def __init__(self):
-        self.keywords = ["tag","cloud"]
-
+def full_scan_create_index():
+    SearchIndex.objects.all().delete() ## remove all search index items
+    alldataset = SysDataset.objects.all()
+    dataset_idx = 0
+    for dataset in alldataset:
+        if dataset_idx%50==0:
+            print("Indexing dataset ", dataset_idx)
+        dataset_idx+=1
+        props = json.loads(dataset.properties)
+        for key in props.keys():
+            if key=="keywords":
+                vals = ""
+                for item in props[key]:
+                    vals+=item+" "
+            else:
+                vals = props[key]
+                
+            terms = vals.lower().split(" ")
+            for term in terms:
+                si = SearchIndex(attribute = key, value = ''.join(e for e in term if e.isalnum()), dataset = dataset)
+                si.save()
 
 @register.simple_tag(takes_context=True)
 def get_tag_cloud_keywords(context, **kwargs):
-    tc = TagCloud()
-    return tc.keywords
+    if SearchIndex.objects.all().count()==0:
+        full_scan_create_index()        
+
+    keywords = list(SearchIndex.objects.filter(attribute='title').values_list('value', flat=True))[:100]
+    keywords = ' '.join(word for word in keywords)
+
+    return keywords
