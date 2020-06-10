08aed3053e3f121f7457b39e5876a9d523f7a196
Hyogi Sim <sandrain@gmail.com>
2020-06-10 11:01:17 -0400

    fix populate script to work with the new container name

--- openshift/mockup-generator/populate.sh
+++ openshift/mockup-generator/populate.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-container="sdidb"
+container="postgresql"
 sqlfile="fakedata.sql"
-
-echo "NOTE: mysqlvolume should be recreated before executing this"
 
 docker cp $sqlfile $container:/tmp
 
-#docker exec -it $container bash -c "mysql -u sdidbp -p sdidb < /tmp/$sqlfile"
 docker exec -it $container bash -c "psql -U sdidbp -d sdidb -f /tmp/$sqlfile"
 
