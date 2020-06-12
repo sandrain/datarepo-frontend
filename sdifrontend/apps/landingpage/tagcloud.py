1d824cca4fdf32652a23d9b751d57989102aafb0
Hyogi Sim <simh@ornl.gov>
2020-06-12 14:42:55 -0400

    connecting search to tagcloud

--- sdifrontend/apps/landingpage/tagcloud.py
+++ sdifrontend/apps/landingpage/tagcloud.py
@@ -21,17 +21,19 @@ def full_scan_create_index():
             else:
                 vals = props[key]
                 
-            terms = vals.lower().split(" ")
-            for term in terms:
-                si = SearchIndex(attribute = key, value = ''.join(e for e in term if e.isalnum()), dataset = dataset)
-                si.save()
+            if key == "keywords":
+                terms = vals.lower().split(" ")
+
+                for term in terms:
+                    si = SearchIndex(attribute = key, value = ''.join(e for e in term if e.isalnum()), dataset = dataset)
+                    si.save()
 
 @register.simple_tag(takes_context=True)
 def get_tag_cloud_keywords(context, **kwargs):
     if SearchIndex.objects.all().count()==0:
         full_scan_create_index()        
 
-    keywords = list(SearchIndex.objects.filter(attribute='title').values_list('value', flat=True))[:100]
+    keywords = list(SearchIndex.objects.filter(attribute='keywords').values_list('value', flat=True))[:100]
     keywords = ' '.join(word for word in keywords)
 
     return keywords
